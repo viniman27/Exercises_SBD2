72981c6c28c769c0daec733ff8deb4473c236cab
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2022-10-28 10:32:42 -0300

    Add files via upload

---
+++ aula1exer2_ViniciusAraujo_200028472/aula1exer2Apaga_ViniciusAraujo_200028472.sql
@@ -0,0 +1,25 @@
+-- --------     << CONTROLE DE VENDAS >>     ------------
+-- 
+--                    SCRIPT DE CRIACAO (DDL)
+-- 
+-- Data Criacao ...........: 28/10/2022
+-- Autor(es) ..............: Vinicius Assumpção de Araújo
+-- Banco de Dados .........: MySQL
+-- Base de Dados(nome) ....: aula1exer2
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 09 Tabelas
+-- 
+-- -----------------------------------------------------------------
+
+USE aula1exer2;
+
+DROP TABLE contem;
+DROP TABLE PRODUTO;
+DROP TABLE VENDA;
+DROP TABLE AREA;
+DROP TABLE supervisiona;
+DROP TABLE telefone;
+DROP TABLE EMPREGADO;
+DROP TABLE GERENTE;
+DROP TABLE PESSOA;
